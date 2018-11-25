a67e06fc10af587c0966fa62d7118d2afc0b32c9
Prakhar Srivastav <srivprakhar@gmail.com>
2018-11-25 23:49:37 +0100

    Working example : list + external property file

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,17 @@
+import com.typesafe.config.ConfigObject;
+
+import java.util.List;
+
 public class Application {
+
+    public static void main(String[] args) {
+        com.typesafe.config.Config conf = Config.load();
+        final String key = conf.getString("main-conf.key");
+        final List<? extends ConfigObject> objectList = conf.getObjectList("main-conf.list");
+        System.out.println("Key is " + key);
+        objectList.stream().forEach(e -> {
+            System.out.println((((ConfigObject) e).get("name").unwrapped()));
+            System.out.println((((ConfigObject) e).get("age")   ));
+        });
+    }
 }
--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,2 +1,12 @@
+import com.typesafe.config.ConfigFactory;
+
+import java.io.File;
+
 public class Config {
+
+    public static com.typesafe.config.Config load(){
+        final File configfile = new File("/home/prakhar/Workspace/java/typesafe-test/config/application.conf");
+        com.typesafe.config.Config config = ConfigFactory.parseFile(configfile);
+        return config;
+    }
 }
